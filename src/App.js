dd9bad7d108173c5a1650175244019376a922a6a
WenHan <wenhantan94@gmail.com>
2022-01-26 11:28:18 +0800

    added waving functionality with the blockchain

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
-// import { ethers } from "ethers";
+import { ethers } from "ethers";
 import './App.css';
+import abi from "./utils/WavePortal.json"
 
 export default function App() {
 
   const [currentAccount, setCurrentAccount] = useState("")
+
+  const contractAddress = "0x8a850a3A520b4293c40f329afffA060e122E1B3c"
+
+  const contractABI = abi.abi
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -47,6 +52,34 @@ const connectWallet = async () => {
   }
 }
 
+const wave = async () => {
+  try {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+      let count = await wavePortalContract.getTotalWaves();
+      console.log("Retrieved total wave count...", count.toNumber());
+
+      const waveTxn = await wavePortalContract.wave();
+      console.log("Mining...", waveTxn.hash);
+
+      await waveTxn.wait();
+      console.log("Mined -- ", waveTxn.hash)
+
+      count = await wavePortalContract.getTotalWaves();
+      console.log("Retrieved total wave count...", count.toNumber())
+    } else {
+      console.log("Ethereum object doesn't exist!");
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
@@ -63,7 +96,7 @@ const connectWallet = async () => {
         I am Wen Han and I am learning solidity? Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={null}>
+        <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
         {!currentAccount && (
